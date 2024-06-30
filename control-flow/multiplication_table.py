b931a23126a152cfd3a78016eee0923d57efaebe
danielkingori <kingndiritu@gmail.com>
2024-06-30 19:29:33 +0300

    mutliply

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+nums = [1,2,3,4,5,6,7,8,9,10]
+number =  int(input("Enter a number to see its multiplication table:"))
+
+for num in nums:
+    result = num * number
+    print(f"{num} * {number} = {result}")
