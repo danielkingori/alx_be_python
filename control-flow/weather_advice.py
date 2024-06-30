12883f32ea4d969005ba1c48d9e3efdd763d86b5
danielkingori <kingndiritu@gmail.com>
2024-06-30 18:10:02 +0300

    debug

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -8,25 +8,3 @@ elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
-
-
-
-
-
-#def main():
-#  """Asks the user for weather conditions and provides clothing recommendations."""
-
-#if __name__ == "__main__":
-#  main()
-  
-  
-#input("What's the weather like today?(sunny/rainy/cold):")
-#
-#if weather == "sunny":
-#    print("Wear a t-shirt and sunglasses.")
-#elif weather == "rainy":
-#    print("Don't forget your umbrella and a raincoat.")
-#elif weather == "cold":
-#    print("Make sure to wear a warm coat and a scarf.")
-#else:
-#    print("Sorry, I don't have recommendations for this weather.")
