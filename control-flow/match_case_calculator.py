0cfeef8f199e6cdac4e1038c58757f3eba36785c
danielkingori <kingndiritu@gmail.com>
2024-06-30 19:05:38 +0300

    match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,3 +1,25 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case '+':
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case '-':
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case '*':
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case '/':
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}.")
+            
+
 #num1 = float(input("Enter the first number:"))  
 #num2 = float(input("Enter the second number:")) 
 #operation = input("Choose the operation(+,-,*,/):")
@@ -41,23 +63,24 @@
 #        else:
 #            result = num1 / num2
 #            print(f"The result is {result}")  
-
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-operation = input("Choose the operation(+,-,*,/): ")
-
-match operation:
-    case "+":
-        result = num1 + num2
-        print(f"The result is {result}")
-    case "-":
-        result = num1 - num2
-        print(f"The result is {result}")
-    case "*":
-        result = num1 * num2
-        print(f"The result is {result}")
-    case "/":
-        result = num1 / num2 if num2 != 0 else "Cannot divide by zero."
-        print(result)  # No need for f-string here as result is either numeric or string
-    case _:
-        print("Invalid operation symbol. Please use +, -, * or /.")
+#
+#num1 = float(input("Enter the first number: "))
+#num2 = float(input("Enter the second number: "))
+#operation = input("Choose the operation(+,-,*,/): ")
+#
+#match operation:
+#    case "+":
+#        result = num1 + num2
+#        print(f"The result is {result}")
+#    case "-":
+#        result = num1 - num2
+#        print(f"The result is {result}")
+#    case "*":
+#        result = num1 * num2
+#        print(f"The result is {result}")
+#    case "/":
+#        result = num1 / num2 if num2 != 0 else "Cannot divide by zero."
+#        print(result)  # No need for f-string here as result is either numeric or string
+#    case _:
+#        print("Invalid operation symbol. Please use +, -, * or /.")
+#
