52259c0441e70119e35d3e0ec9953850b70ba0c6
danielkingori <kingndiritu@gmail.com>
2024-06-30 18:49:55 +0300

    mc

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,16 +1,26 @@
-#num1 = input("Enter the first number:")
-#num2 = input("Enter the second number:")
+#num1 = float(input("Enter the first number:"))  
+#num2 = float(input("Enter the second number:")) 
 #operation = input("Choose the operation(+,-,*,/):")
+#
 #match operation:
 #    case "+":
 #        result = num1 + num2
-#        print("The result is "+ result)
+#        print(f"The result is {result}")  
+#    case "-":
+#        result = num1 - num2
+#        print(f"The result is {result}")  
 #    case "*":
 #        result = num1 * num2
-#        print("The result is "+ result)
+#        print(f"The result is {result}")  
+#    case "/":
+#        if num2 == 0:
+#            print("Cannot divide by zero.")
+#        else:
+#            result = num1 / num2
+#            print(f"The result is {result}")  
+#   # case _:
+#    #    print("Invalid operation symbol. Please use +, -, * or /.")
 #
-
-
 num1 = float(input("Enter the first number:"))  
 num2 = float(input("Enter the second number:")) 
 operation = input("Choose the operation(+,-,*,/):")
@@ -31,5 +41,3 @@ match operation:
         else:
             result = num1 / num2
             print(f"The result is {result}")  
-   # case _:
-    #    print("Invalid operation symbol. Please use +, -, * or /.")
