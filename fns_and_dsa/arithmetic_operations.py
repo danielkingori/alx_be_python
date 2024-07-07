0feba21a686692a77675e85972a182c147d2f5b4
danielkingori <kingndiritu@gmail.com>
2024-07-07 15:23:05 +0300

    arithimetic

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,33 @@
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        return num1 / num2
+    else:
+        print("not applicable:Either use add, subtract, multiply, divide") 
+    
+    
+#    def add():
+#        return num1 + num2
+#    def subtract(num1, num2):
+#        return num1 - num2
+#    def multiply(num1, num2):
+#        return num1 * num2
+#    def divide(num1, num2): 
+#        return num1 / num2
+
+
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
+
+main()
