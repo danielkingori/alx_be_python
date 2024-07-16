05ce23bee5d11a6bea5354967bf35187c7017a6f
danielkingori <kingndiritu@gmail.com>
2024-07-16 20:57:27 +0300

    inheritance

--- programming_paradigm/learn1.py
+++ programming_paradigm/learn1.py
@@ -1,4 +1,4 @@
-#encapsulation - a principle in OOP that allows methods and attributes to be infused /bundled inside a class
+#encapsulation - a principle in OOP that allows methods and attributes to be infused/bundled inside a class
 class Person:
     #instantiate the class and add properties using the init method
     def __init__(self, name, age):
@@ -15,3 +15,31 @@ class Person:
 person1 = Person("jon", 30)
 print(person1.walk())
 print(person1)
+
+
+#inheritance - principle in OOP that allows for class or subclass that inherits methods and attributes from existing super class
+
+#object is an instance of a class
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        #super - special function used to inherit from a super/base class 
+        #gives you access to the parent class
+        super().__init__(name, age)
+        self.student_id = student_id
+        self.name = name
+        self.age = age
+        self.course = []
+        
+    def enrol(self, course):
+        self.course.append(course)
+        return f"{self.name} enrolled in {self.course}"
+    
+    
+    def __str__(self):
+        return f"{self.name}, {self.age}, {self.student_id}, {self.walk()}"
+        
+Student1 = Student("henry", 40, 2)
+print(Student1)
+#print(Student1.walk())
+print(Student1.enrol("Maths"))
+
