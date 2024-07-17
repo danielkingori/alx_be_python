6493f4839c2c9d01b05eead4e07e83a6e13d2a01
danielkingori <kingndiritu@gmail.com>
2024-07-17 22:24:51 +0300

    helo

--- oop/moreoop.py
+++ oop/moreoop.py
@@ -4,35 +4,59 @@
 
 #self - variable that refers to current instance
 class Point:
+    count = 0
     #called when we create an object
     def __init__(self, value_one, value_two): #PEP - coding conventions
         self.x = value_one
         self.y = value_two
         
-    def __str__(self):
-        return f"( x={self.x}, y={self.y} )"
+    # def __str__(self):
+    #     return f"( x={self.x}, y={self.y} )"
     
-    def __del__(self):
-        print(f"Object {self} has been destroyed")
+    # def __del__(self):
+    #     print(f"Object {self} has been destroyed")
     
     #research
     #def __
+    
+    #instance method , instance attributes
+    def hello(self):
+        pass
+
+    @classmethod
+    def hello2(cls):
+        print(f"The value of cls is {cls}")
+    
 #creating objects/occurence/instance of a class 
-point1 = Point(1,1)
-point2 = Point(3,2.5)
+# point1 = Point(1,1)
+# point2 = Point(3,2.5)
 
-print(point2)
-del point2
-print(point2)
+# print(point2)
+# del point2
+# print(point2)
 
-#instance method , instance attributes
-def hello(self):
-    pass
 
-@classmethod
-def hello2(cls):
-    print(f"The value of cls is {cls}")
 
 #class methods - works with class attributes - work with class methods like count
 
 #@class method decorator, turns our class into a class method
+
+class MyClass:
+    class_variable = 'class variable'
+
+    @classmethod
+    def class_method(cls):
+        print(f'Called class_method of {cls}')
+        print(cls.class_variable)
+
+MyClass.class_method()  # This is how you call a class method
+obj = MyClass()
+obj.class_method()  # This also works
+
+
+class MyClass:
+    def instance_method(self):
+        print(f'Called instance_method of {self}')
+
+obj = MyClass()
+obj.instance_method()  # This is how you call an instance method
