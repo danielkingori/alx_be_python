576e68ec9fbb85c9fdbcb0ca1d7efa40d606f452
danielkingori <kingndiritu@gmail.com>
2024-07-21 23:57:26 +0300

    library

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,28 +1,50 @@
+
 class Book:
-    def __init__(self,title, author):
+    def __init__(self, title, author ):
         self.title = title
         self.author = author
+        
     def __str__(self):
-        return f"{self.title} by {self.author}"
-book1 = Book("things fall apart", "chinua")
-book2 = Book("things sit bach apart", "linus")
-book3 = Book("mool fall apart", "ton")
-print(book1)
-print(book2)
-print(book3)
+        return f"{self.title} by {self.author}"    
+        
+book1 = Book("Book: Pride and Prejudice", "Jane Austen")
+print (book1)
+        # print(f"Book: ", self.title, " by " , self.author) 
+        # pass
 
-class Member:
-    def __init__(self, name):
-        self.name = name
-        self.borrowed_books = []
-    def borrow_books(self, book):
-        self.borrowed_books.append(book)
-    def __str__(self):
-        return f"member: {self.name} borrowed books: {[str(book) for book in self.borrowed_books]}"
-member1 = Member("simon")
-member2 = Member("Kot")
+class EBook(Book):
+    def __init__(self, title, author, file_size ):
+        super().__init__(title, author)
+        self.file_size = file_size
+        print(f"EBook: {title} by {author}, File Size: {file_size}KB")
+        pass
+        
+        
+class PrintBook(Book):
+    def __init__(self, title, author, page_count ):
+        super().__init__(title, author)
+        self.page_count = page_count
+        print(f"PrintBook: {self.title} by {self.author}, Page Count: {page_count}")
+        pass
 
-member1.borrow_books(book1)
-member2.borrow_books(book2)
 
-print(member1,member2)
+class Library():
+    def __init__(self):
+        self.books = []
+        
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book) 
+
+        if isinstance(book, EBook):
+            self.books.append(book) 
+
+        elif isinstance(book, PrintBook):
+            self.books.append(book)      
+
+        else:
+            pass
+
+    def list_books(self):
+        for book in self.books:
+            return book
