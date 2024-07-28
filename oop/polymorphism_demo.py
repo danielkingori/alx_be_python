48cb7ce006b710b785071e833a3e822efb26db12
danielkingori <kingndiritu@gmail.com>
2024-07-28 11:36:13 +0300

    polymorphism

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,3 +1,26 @@
 class Shape:
-    def __init__(self) -> None:
-        pass
+    def area(self):
+        pass    
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+            
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+            
+    def area(self):
+        import math
+        return math.pi * (self.radius**2)
+        
+rect = Rectangle(5,12)
+print(f"The area of the rectangle is : {rect.area()}")
+    
+circ = Circle(3)
+print(f"The area of a circle is: {circ.area()}")
+ 
+    
