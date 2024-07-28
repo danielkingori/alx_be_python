f6bfa3ed77b8c52e01a286c55ba459f4b300548a
danielkingori <kingndiritu@gmail.com>
2024-07-28 12:35:18 +0300

    class static

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,3 +1,20 @@
 class Calculator:
-    def __init__(self) -> None:
-        pass
+    calculation_type = "Arithimetic Operations"
+    # def __init__(self) -> None:
+    #     pass
+    
+    @staticmethod    
+    def add(a,b):
+        return a + b
+    
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
+
+sum_result = Calculator.add(10,5)
+print(f"The sum is: {sum_result}")
+
+product_result = Calculator.multiply(10,5)
+print(f"The product is: {product_result}")
+    
